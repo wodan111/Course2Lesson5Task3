cfb554b62753b75fee6b83fe69616606953253ed
Vadim <testov@test.ua>
2019-01-02 22:18:58 +0200

    .

--- Course2Lesson5Task3/src/ua/testov/test/Group.java
+++ Course2Lesson5Task3/src/ua/testov/test/Group.java
@@ -142,10 +142,10 @@ public class Group implements Military {
 			for (int i = 0; i < this.st.length; i++) {
 //		str+=st[i].getFirstName()+" "+st[i].getSecondName()+" "+st[i].isGender()+" "+st[i].getAge()+" "+st[i].getNumberOfGradebook()+" "+
 //				st[i].isStipend()+" "+st[i].getIncome();
-				if (st[i] != null) {
-					str += this.st[i].getNumberOfGradebook() + "," + this.st[i].getSecondName() + ","
-							+ this.st[i].isGender() + "," + this.st[i].getAge();
-					str += System.lineSeparator();
+				if(st[i]!=null) {
+				str += this.st[i].getNumberOfGradebook() + "," + this.st[i].getSecondName() + ","
+						+ this.st[i].isGender() + "," + this.st[i].getAge();
+				str+=System.lineSeparator();
 				}
 			}
 		} catch (NullPointerException e) {
@@ -155,6 +155,28 @@ public class Group implements Military {
 
 		return saveStringToFile(str, new File(path, "result.csv"));
 	}
+	
+//	public static String readTextFromFile(File file) {
+//		String str = "";
+//		String strRes = "";
+//		try (BufferedReader f = new BufferedReader(new FileReader(file))) {
+//			for (; (str = f.readLine()) != null;) {
+//				strRes += str;
+//				strRes +=System.lineSeparator();
+//			}
+//		} catch (IOException e) {
+//			System.out.println("ERROR");
+//		}
+//		return strRes;
+//	}
+//	public static String[] groupFromFile(String path) {
+//		File gr=new File(path);
+//		String[] massOne = readTextFromFile(gr).split(",");
+//		for (int i = 0; i < massOne.length; i++) {
+//			Student student=new Student();
+//		}
+//		return massOne;
+//	}
 
 	@Override
 	public String toString() {
